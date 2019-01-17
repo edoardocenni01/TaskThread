7782f43a2f10ac504de701074acca1e52e87e855
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-17 11:09:34 +0100

    Avviati i Task

---
+++ WpfAppTask/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfAppTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        private CancellationTokenSource token = new CancellationTokenSource();
+
+
+        private void btnStart1_Click(object sender, RoutedEventArgs e)
+        {
+            if (token == null)
+                token = new CancellationTokenSource();
+            Task.Factory.StartNew(() => Conteggio(token, 10000, lblRis));
+        }
+
+        private void Conteggio(CancellationTokenSource token, int max, Label lbl)
+        {
+            for (int i = 0; i < max; i++)
+            {
+                Dispatcher.Invoke(() => AggiornaUI(i, lbl));
+                Thread.Sleep(500);
+                Dispatcher.Invoke(() => AggiornaUI1(lbl));
+                Thread.Sleep(500);
+                if (token.Token.IsCancellationRequested)
+                    break;
+            }
+            Dispatcher.Invoke(() => AggiornaUI2(lbl));
+        }
+
+        private void AggiornaUI2(Label lbl)
+        {
+            lbl.Content = "Ho finito!";
+        }
+
+        private void AggiornaUI(int i, Label lbl)
+        {
+            lbl.Content = $"Sto contando...{i.ToString()}";
+        }
+
+        private void AggiornaUI1(Label lbl)
+        {
+            lbl.Content = "Sto aspettando... !";
+        }
+
+
+    }
+}
